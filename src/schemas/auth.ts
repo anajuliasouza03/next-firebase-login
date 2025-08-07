bc86f992cc95afaea9df7d0f0252808285fc7ad1
Ana Júlia Souza <anajuliapereiradesouza003@gmail.com>
2025-08-07 11:20:40 -0400

    feat(auth): configure Firebase and Zod schemas

---
+++ src/schemas/auth.ts
@@ -0,0 +1,29 @@
+import { register } from "module";
+import { email, z } from "zod";
+
+//login
+export const loginSchema = z.object({
+  email: z.string().email({ message: "Email Inválido" }),
+  password: z.string().min(6, { message: "Mínimo de 6 caracteres" }),
+});
+export type LoginInput = z.infer<typeof loginSchema>;
+
+//reset
+export const resetSchema = z.object({
+  email: z.string().email({ message: "Email inválido" }),
+});
+export type ResetInput = z.infer<typeof resetSchema>;
+
+//register
+export const registerSchema = z
+  .object({
+    name: z.string().min(2, { message: "Informe seu nome" }),
+    email: z.string().email({ message: "Email inválido!" }),
+    password: z.string().min(6, { message: "Mínimo de 6 caracteres" }),
+    confirmPassword: z.string().min(6, { message: "Confirmacao necessária" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não batem",
+    path: ["confirmPassword"],
+  });
+export type RegisterInput = z.infer<typeof register>;
